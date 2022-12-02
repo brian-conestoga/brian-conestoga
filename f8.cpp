148ba8bac5f06521c3a35fc80c204ba5eece8d10
Brian Nguyen <bnguyen9850@conestogac.on.ca>
2022-12-02 18:51:38 -0500

    added struct declaration

---
+++ f8.cpp
@@ -0,0 +1,21 @@
+/*
+* Filename: f8.cpp
+* Project: Focused8
+* By: Brian Nguyen
+* Date: December 2 2022
+* Description:a program to give you practice at working with C-style strings, files, command-line
+arguments, and structs
+*/
+
+#pragma warning(disable: 4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct MyData
+{
+	int howMany;
+	char theText[21];
+	char directoryPath[80];
+	char filename[80];
+};
